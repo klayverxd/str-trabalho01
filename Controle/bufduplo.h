5b8c31857f58fa64ed67de7a968cb5702f3a249d
Klayver Ximenes <klayverx@gmail.com>
2022-01-08 16:12:30 -0300

    feat: grava dados dos sensores com buffer duplo

---
+++ Controle/bufduplo.h
@@ -0,0 +1,9 @@
+#ifndef BUFDUPLO_H
+#define BUFDUPLO_H
+
+void bufduplo_insere_leitura(long leitura);
+long* bufduplo_espera_buffer_cheio(void);
+int bufamostras(void);
+int get_tam_buffer(void);
+
+#endif
