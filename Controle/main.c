13b943a648702e41ff1bb4fbeb25f1bce39773b4
Klayver Ximenes <klayverx@gmail.com>
2022-01-11 11:25:25 -0300

    feat: adiciona input de temp. e nivel de referencia

--- Controle/main.c
+++ Controle/main.c
@@ -11,8 +11,9 @@
 
 #define NSEC_PER_SEC (1000000000)
 #define NUM_AMOSTRAS 1000
-#define LIMIT_ALARME 30
-#define TEMP_REF 29
+#define LIMITE_ALARME_TEMP 30
+
+double TEMP_REF = 0.0, NIVEL_REF = 0.0;
 
 // MOSTRA OS DADOS PERIODICAMENTE
 void thread_mostra_status(void) {
@@ -29,13 +30,15 @@ void thread_mostra_status(void) {
     aloca_tela();
     // apresentação dos valores em tela
     system("tput reset");
-    printf("=============================\n");
+    printf("(T_R) Temperatura referencia => %.2lf\n", TEMP_REF);
+    printf("(H_R) Nivel referencia => %.2lf\n", NIVEL_REF);
+    printf("=======================================\n");
     printf("(T) Temperatura da agua => %.2lf\n", t);
     printf("(Ta) Temp. ar ambiente => %.2lf\n", ta);
-    printf("(Ti) Temp. agua que entra no recipiente => %.2lf\n", ti);
+    printf("(Ti) Temp. agua que entra => %.2lf\n", ti);
     printf("(No) Fluxo de saida => %.2lf\n", no);
     printf("(H) Nivel de agua => %.2lf\n", h);
-    printf("=============================\n");
+    printf("=======================================\n");
     libera_tela();
 
     sleep(1);
@@ -68,7 +71,7 @@ void thread_le_sensor(void) {
 
 void thread_alarme(void) {
   while (1) {
-    sensor_alarme_temp(LIMIT_ALARME);
+    sensor_alarme_temp(LIMITE_ALARME_TEMP);
 
     aloca_tela();
     printf("!!! Temperatura limite atingida !!!\n");
@@ -152,6 +155,75 @@ void thread_controle_temperatura(void) {
   }
 }
 
+void thread_controle_nivel(void) {
+  char msg_enviada[1000];
+  double temp, ref_temp;
+
+  struct timespec t, t_fim;
+  long periodo = 50e6;  // 50ms
+  long atraso_fim; // momento em que inicia até o fim
+
+  // leitura da hora atual
+  clock_gettime(CLOCK_MONOTONIC, &t);
+
+  while (1) {
+    // espera até o início do proximo periodo
+    clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &t, NULL);
+
+    temp = get_sensor("t");
+    ref_temp = get_ref_temp();
+
+    // Ni -> quantidade de água que entra no sistema
+    // Na -> quantidade de água aquecida que entra no sistema (80°C)
+    double ni, na;
+
+    if (temp > ref_temp) {
+      // abre todo o atuador Ni (entrada de agua natural)
+      sprintf(msg_enviada, "ani%lf", 100.0);
+      msg_socket(msg_enviada);
+
+      // abre todo o atuador Nf (valvula de liberacao)
+      sprintf(msg_enviada, "anf%lf", 100.0);
+      msg_socket(msg_enviada);
+
+      // fecha o atuador Na (entrada de agua aquecida)
+      sprintf(msg_enviada, "ana%lf", 0.0);
+      msg_socket(msg_enviada);
+    }
+
+    if (temp < ref_temp) {
+      // controle proporcional ao erro
+      if ((ref_temp - temp) * 20 > 10.0)
+        // Atuador (Na) no máximo
+        na = 10.0;
+      else
+        na = (ref_temp - temp) * 20;
+
+      // fecha o atuador Ni (entrada de agua natural)
+      sprintf(msg_enviada, "ani%lf", 0.0);
+      msg_socket(msg_enviada);
+
+      // Fica em 10 o atuador Nf (valvula de liberacao)
+      sprintf(msg_enviada, "anf%lf", 10.0);
+      msg_socket(msg_enviada);
+
+      // Atuador (Na) proporcional ao erro atual
+      sprintf(msg_enviada, "ana%lf", na);
+      msg_socket(msg_enviada);
+    }
+
+    // leitura da hora atual
+    clock_gettime(CLOCK_MONOTONIC, &t_fim);
+
+    // calcula inicio do prox periodo
+    t.tv_nsec += periodo;
+    while (t.tv_nsec >= NSEC_PER_SEC) {
+      t.tv_nsec -= NSEC_PER_SEC;
+      t.tv_sec++;
+    }
+  }
+}
+
 void thread_grava_temp_resp(void) {
   FILE* dados_f;
   dados_f = fopen("dados_sensores.txt", "w");
@@ -184,22 +256,38 @@ void thread_grava_temp_resp(void) {
   fclose(dados_f);
 }
 
-void main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {
+  system("tput reset");
+  while (TEMP_REF <= 0.0) {
+    printf("Digite um valor para a temperatura de referência maior que 0: ");
+    scanf("%lf", &TEMP_REF);
+    system("tput reset");
+  }
+
+  while (NIVEL_REF <= 0.0) {
+    printf("Digite um valor para o nível de referência maior que 0: ");
+    scanf("%lf", &NIVEL_REF);
+    system("tput reset");
+  }
+
   put_ref_temp(TEMP_REF);
 
   int porta_destino = atoi(argv[2]);
-  // função para criar o canal de comunicação via rede
+  // cria o canal de comunicação via rede
   cria_socket(argv[1], porta_destino);
 
-  pthread_t t1, t2, t3, t4, t5;
+  pthread_t t1, t2, t3, t4, t5, t6;
 
   pthread_create(&t1, NULL, (void*)thread_mostra_status, NULL);
   pthread_create(&t2, NULL, (void*)thread_le_sensor, NULL);
   pthread_create(&t3, NULL, (void*)thread_alarme, NULL);
   pthread_create(&t4, NULL, (void*)thread_controle_temperatura, NULL);
-  pthread_create(&t5, NULL, (void*)thread_grava_temp_resp, NULL);
+  pthread_create(&t5, NULL, (void*)thread_controle_nivel, NULL);
+  pthread_create(&t6, NULL, (void*)thread_grava_temp_resp, NULL);
 
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
   pthread_join(t3, NULL);
+
+  return 0;
 }
